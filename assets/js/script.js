0e20bc803d7cf9975cf2f5dde4dbc3f1760b2a99
RNXVIII <>
2024-01-22 23:18:06 +0000

    added code to increment the score for right and wrong awnsers

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,13 +81,26 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+
 }
+
 
 function displayAdditionQuestion(operand1, operand2) {
 
